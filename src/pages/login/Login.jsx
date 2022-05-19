178fdbf820e7c9a037dbe3b55c5a605b605080c9
kodaishiotsuki <0711dr5@gmail.com>
2022-05-19 11:44:38 +0900

    ログイン画面作成

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,5 +2,24 @@ import React from "react";
 import "./Login.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className='login'>
+      <div className='loginWrapper'>
+        <div className='loginLeft'>
+          <h3 className='loginLogo'>MERN_01</h3>
+          <span className='loginDesc'>MERNスタックエンジニア</span>
+        </div>
+        <div className='loginRight'>
+          <div className='loginBox'>
+            <p className='loginMsg'>ログインはこちら</p>
+            <input type='text' className='loginInput' placeholder='email' />
+            <input type='text' className='loginInput' placeholder='password' />
+            <button className="loginButton">ログイン</button>
+            <span className="loginForgot">パスワードを忘れた方へ</span>
+            <button className="loginRegisterButton">アカウント作成</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
